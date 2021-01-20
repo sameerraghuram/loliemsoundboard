a72e459324f2fff9636c96b02b2ed68ec4f00886
Hriday96 <hridayjhaveri@gmail.com>
2021-01-20 03:00:09 +0100

    Added functionality to delete webhook message after sending(Testing required)

---
+++ hook.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+const config = {
+    webhookID: '800560313656934432',
+    webhookToken: 'zCuPPENSTyes-NkjMnR5HFY7H5WXylEl0i8oyDh9_9KtDD3e81WU-XnKXvCnXF7TIWIO',
+    clientId: '317641730708996107'
+};
+const webhookClient = new Discord.WebhookClient(config.webhookID, config.webhookToken);
+
+webhookClient.send('!play tunaa', {
+    username: config.clientId,
+    avatarURL: 'https://i.imgur.com/wSTFkRM.png',
+}).then(() => webhookClient.delete);
